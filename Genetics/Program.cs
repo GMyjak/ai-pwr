0495ada49eb884c52a134e61f9ce388b18e9fcac
Grzegorz Myjak <g-myjak@wp.pl>
2021-03-04 18:25:55 +0100

    Initial commit

---
+++ Genetics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Genetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
